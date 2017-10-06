e68afcb10d7b10152f2558ed5c36da4d3162d9af
ANAGHA-R-NAIR <31248818+ANAGHA-R-NAIR@users.noreply.github.com>
2017-10-06 18:29:32 +0530

    Add files via upload

---
+++ 4b_q01.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int calPower(int, int);
+int main()
+{
+    int base, power, result;
+
+    cout << "Enter base number: ";
+    cin >> base;
+
+    cout << "Enter power number(positive integer): ";
+    cin >> power;
+
+    result = calPower(base, power);
+    cout << base << "^" << power << " = " << result;
+    return 0;
+}
+int calPower(int base, int power)
+{
+    if (power != 0)
+        return (base*calPower(base, power-1));
+    else
+        return 1;
+}
